ae41ac807a7f0ae84d2a42c953b208b71e4c4580
niigxzcsc <nicoly.donadi@escola.pr.gov.br>
2025-06-13 09:01:43 -0300

    cartoes_13-06-2025

---
+++ sketch.js
@@ -0,0 +1,14 @@
+criaCartao(
+'Programacao',
+'O que e JavaScript?',
+'JavaScript e uma linguagem de programacao.'
+);
+criaCartao(
+'Geografia',
+'Qual a capital da Franca?',
+'A capital da Franca e Paris.'
+);
+criaCartao('Programacao',
+'O que e uma funcao?',
+'Uma funcao e um bloco de codigo que executa uma tarefa.'
+);
